0b94eadbca3d22a9cd83130bf57e952696aff9f1
max7171713 <116950511+max7171713@users.noreply.github.com>
2023-04-15 17:53:35 +0700

    Add files via upload

---
+++ ау.py
@@ -0,0 +1,10 @@
+class NPC:
+    def __init(self,a,b,c,d,e,f):
+        self.name = a
+        self.rost = b
+        self.hp = c
+        self.professia = d
+        self.tovar = e
+        self.uslugi = f
+    def name(self):
+        print("меня зовут игорь")
