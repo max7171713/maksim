250f8c8b30f3a1b56c9abab27c5f1bd165e3e180
max7171713 <116950511+max7171713@users.noreply.github.com>
2023-05-06 18:00:45 +0700

    Add files via upload

---
+++ выборы 1 сезон (2 сезона не будит не будит).py
@@ -0,0 +1,12 @@
+command = input()
+while command != "выход":
+    if command == "начать игру":
+        print('игра началась')
+        print('пожалуйста выбирете что хотите сейчас')
+        print("играть")
+        print("выход")
+        
+        
+        
+        
+    command=input()
