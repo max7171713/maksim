026c1e187dfe3f9c5d0bcc68a82f3c22366fffe3
max7171713 <116950511+max7171713@users.noreply.github.com>
2024-03-16 18:04:52 +0700

    Add files via upload

---
+++ авыа.py
@@ -0,0 +1,6 @@
+my_list = []
+for i in range(0,8,+1):
+    a = input("что вы хотите добавить в список")
+    my_list.append(a)
+    print("вот что у вас в списке:",my_list)
+print("а у вас есть в списке молоко?")
---
+++ пкпукпукпукпкупу.py
@@ -0,0 +1,8 @@
+m = []
+a = 0
+for i in range(0,8,+1):
+    h = int(input("напишите число"))
+    m.append(h)
+for m in m:
+    a = a+m
+print(a)
