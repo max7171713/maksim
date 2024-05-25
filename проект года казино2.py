8542ecfb9fa21800ee1aba510d5b76372158b30c
max7171713 <116950511+max7171713@users.noreply.github.com>
2024-05-25 16:29:35 +0700

    Update проект года казино2.py

--- проект года казино2.py
+++ проект года казино2.py
@@ -17,24 +17,66 @@ def kazik(event):
     elif o <= 299:
         if yuy <= 0:
             oi("это казино тут есть ставки выигрышь проигрышь главное не потеряйте все деньги")
-            oi(f"ваш баланс = {balanse}")
-            oi("максимум ставка 40000 минимум 300")
+            oi(f"ваш баланс = {balanse}$")
+            oi("максимум ставка 100000$ минимум 300$")
             oi("а так же будут шансы на выигрышь")
-            ui(f"ваш баланс = {balanse}")
             yuy = yuy+1
-        ui("простите но минимальная ставка это 300")
-    elif o >= 1001 and o <= 40000:
+        ui("простите но минимальная ставка это 300$")
+    elif o > 40000:
         if yuy <= 0:
             oi("это казино тут есть ставки выигрышь проигрышь главное не потеряйте все деньги")
-            oi(f"ваш баланс = {balanse}")
-            oi("максимум ставка 40000 минимум 300")
+            oi(f"ваш баланс = {balanse}$")
+            oi("максимум ставка 1000000$ минимум 300$")
             oi("а так же будут шансы на выигрышь")
-            oi(f"ваш баланс = {balanse}")
             yuy = yuy+1
         if balanse < o:
             oi("простите но вы не можете поставить эту ставку")
         else:
-            oi(f"ваша ставка = {o}")
+            oi(f"ваша ставка = {o}$")
+            g = random.randint(1,3)
+            g = g*10
+            opp = random.randint(1,10)
+            oi(f"ваш шанс выиграть = {g}%")
+            g = g/10
+            if g == 1:
+                if opp <= 1:
+                    oi(f"вы выиграли {o*2}$!")
+                    balanse = o*2+balanse
+                    oi(f"ваш баланс = {balanse}$")
+                else:
+                    oi(f"вы проиграли {o}$")
+                    balanse = balanse - o
+                    oi(f"ваш баланс = {balanse}$")
+            if g == 2:
+                if opp <= 2:
+                    oi(f"вы выиграли {o*2}$!")
+                    balanse = o*2+balanse
+                    oi(f"ваш баланс = {balanse}$")
+                else:
+                    oi(f"вы проиграли {o}$")
+                    balanse = balanse - o
+                    oi(f"ваш баланс = {balanse}$")
+            if g == 3:
+                if opp <= 3:
+                    oi(f"вы выиграли {o*2}$!")
+                    balanse = o*2+balanse
+                    oi(f"ваш баланс = {balanse}$")
+                else:
+                    oi(f"вы проиграли {o}$")
+                    balanse = balanse - o
+                    oi(f"ваш баланс = {balanse}$")
+                    
+    elif o >= 1001 and o <= 40000:
+        if yuy <= 0:
+            oi("это казино тут есть ставки выигрышь проигрышь главное не потеряйте все деньги")
+            oi(f"ваш баланс = {balanse}$")
+            oi("максимум ставка 1000000$ минимум 300$")
+            oi("а так же будут шансы на выигрышь")
+            yuy = yuy+1
+        if balanse < o:
+            oi("простите но вы не можете поставить эту ставку")
+        else:
+            oi(f"ваша ставка = {o}$")
             g = random.randint(1,5)
             g = g*10
             opp = random.randint(1,10)
@@ -42,72 +84,72 @@ def kazik(event):
             g = g/10
             if g == 1:
                 if opp <= 1:
-                    oi(f"вы выиграли {o*2}!")
+                    oi(f"вы выиграли {o*2}$!")
                     balanse = o*2+balanse
-                    oi(f"ваш баланс = {balanse}")
+                    oi(f"ваш баланс = {balanse}$")
                 else:
-                    oi(f"вы проиграли {o}")
+                    oi(f"вы проиграли {o}$")
                     balanse = balanse - o
-                    oi(f"ваш баланс = {balanse}")
+                    oi(f"ваш баланс = {balanse}$")
             if g == 2:
                 if opp <= 2:
-                    oi(f"вы выиграли {o*2}!")
+                    oi(f"вы выиграли {o*2}$!")
                     balanse = o*2+balanse
-                    oi(f"ваш баланс = {balanse}")
+                    oi(f"ваш баланс = {balanse}$")
                 else:
-                    oi(f"вы проиграли {o}")
+                    oi(f"вы проиграли {o}$")
                     balanse = balanse - o
-                    oi(f"ваш баланс = {balanse}")
+                    oi(f"ваш баланс = {balanse}$")
             if g == 3:
                 if opp <= 3:
-                    oi(f"вы выиграли {o*2}!")
+                    oi(f"вы выиграли {o*2}$!")
                     balanse = o*2+balanse
-                    oi(f"ваш баланс = {balanse}")
+                    oi(f"ваш баланс = {balanse}$")
                 else:
-                    oi(f"вы проиграли {o}")
+                    oi(f"вы проиграли {o}$")
                     balanse = balanse - o
-                    oi(f"ваш баланс = {balanse}")
+                    oi(f"ваш баланс = {balanse}$")
             if g == 4:
                 if opp <= 4:
-                    oi(f"вы выиграли {o*2}!")
+                    oi(f"вы выиграли {o*2}$!")
                     balanse = o*2+balanse
-                    oi(f"ваш баланс = {balanse}")
+                    oi(f"ваш баланс = {balanse}$")
                 else:
-                    oi(f"вы проиграли {o}")
+                    oi(f"вы проиграли {o}$")
                     balanse = balanse - o
-                    oi(f"ваш баланс = {balanse}")
+                    oi(f"ваш баланс = {balanse}$")
             if g == 5:
                 if opp <= 5:
-                    oi(f"вы выиграли {o*2}!")
+                    oi(f"вы выиграли {o*2}$!")
                     balanse = o*2+balanse
-                    oi(f"ваш баланс = {balanse}")
+                    oi(f"ваш баланс = {balanse}$")
                 else:
-                    oi(f"вы проиграли {o}")
+                    oi(f"вы проиграли {o}$")
                     balanse = balanse - o
-                    oi(f"ваш баланс = {balanse}")
+                    oi(f"ваш баланс = {balanse}$")
             
-    elif o >= 40001:
+    elif o >= 100001:
         if yuy <= 0:
             oi("это казино тут есть ставки выигрышь проигрышь главное не потеряйте все деньги")
-            oi(f"ваш баланс = {balanse}")
-            oi("максимум ставка 40000 минимум 300")
+            oi(f"ваш баланс = {balanse}$")
+            oi("максимум ставка 100000$ минимум 300$")
             oi("а так же будут шансы на выигрышь")
-            oi(f"ваш баланс = {balanse}")
+            oi(f"ваш баланс = {balanse}$")
             yuy = yuy+1
         oi("простите но казино не принимает такие ставки")
      
-    elif o == 300 or o == 1000 or o == 400 or o == 500 or o == 600 or o == 700 or o == 800 or o == 900:
+    elif o >= 300 and o <= 1000:
         if yuy <= 0:
             oi("это казино тут есть ставки выигрышь проигрышь главное не потеряйте все деньги")
-            oi(f"ваш баланс = {balanse}")
-            oi("максимум ставка 40000 минимум 300")
+            oi(f"ваш баланс = {balanse}$")
+            oi("максимум ставка 100000$ минимум 300$")
             oi("а так же будут шансы на выигрышь")
-            oi(f"ваш баланс = {balanse}")
+            oi(f"ваш баланс = {balanse}$")
             yuy = yuy+1
         if balanse < o:
             oi("простите но вы не можете поставить эту ставку")
         else:
-            oi(f"ваша ставка = {o}")
+            oi(f"ваша ставка = {o}$")
             g = random.randint(1,8)
             g = g*10
             opp = random.randint(1,10)
@@ -115,76 +157,76 @@ def kazik(event):
             g = g/10
             if g == 1:
                 if opp <= 1:
-                    oi(f"вы выиграли {o*2}!")
+                    oi(f"вы выиграли {o*2}$!")
                     balanse = o*2+balanse
-                    oi(f"ваш баланс = {balanse}")
+                    oi(f"ваш баланс = {balanse}$")
                 else:
-                    oi(f"вы проиграли {o}")
+                    oi(f"вы проиграли {o}$")
                     balanse = balanse - o
-                    oi(f"ваш баланс = {balanse}")
+                    oi(f"ваш баланс = {balanse}$")
             if g == 2:
                 if opp <= 2:
-                    oi(f"вы выиграли {o*2}!")
+                    oi(f"вы выиграли {o*2}$!")
                     balanse = o*2+balanse
-                    oi(f"ваш баланс = {balanse}")
+                    oi(f"ваш баланс = {balanse}$")
                 else:
-                    oi(f"вы проиграли {o}")
+                    oi(f"вы проиграли {o}$")
                     balanse = balanse - o
-                    oi(f"ваш баланс = {balanse}")
+                    oi(f"ваш баланс = {balanse}$")
             if g == 3:
                 if opp <= 3:
-                    oi(f"вы выиграли {o*2}!")
+                    oi(f"вы выиграли {o*2}$!")
                     balanse = o*2+balanse
-                    oi(f"ваш баланс = {balanse}")
+                    oi(f"ваш баланс = {balanse}$")
                 else:
-                    oi(f"вы проиграли {o}")
+                    oi(f"вы проиграли {o}$")
                     balanse = balanse - o
-                    oi(f"ваш баланс = {balanse}")
+                    oi(f"ваш баланс = {balanse}$")
             if g == 4:
                 if opp <= 4:
-                    oi(f"вы выиграли {o*2}!")
+                    oi(f"вы выиграли {o*2}$!")
                     balanse = o*2+balanse
-                    oi(f"ваш баланс = {balanse}")
+                    oi(f"ваш баланс = {balanse}$")
                 else:
-                    oi(f"вы проиграли {o}")
+                    oi(f"вы проиграли {o}$")
                     balanse = balanse - o
-                    oi(f"ваш баланс = {balanse}")
+                    oi(f"ваш баланс = {balanse}$")
             if g == 5:
                 if opp <= 5:
-                    oi(f"вы выиграли {o*2}!")
+                    oi(f"вы выиграли {o*2}$!")
                     balanse = o*2+balanse
-                    oi(f"ваш баланс = {balanse}")
+                    oi(f"ваш баланс = {balanse}$")
                 else:
-                    oi(f"вы проиграли {o}")
+                    oi(f"вы проиграли {o}$")
                     balanse = balanse - o
-                    oi(f"ваш баланс = {balanse}")
+                    oi(f"ваш баланс = {balanse}$")
             if g == 6:
                 if opp <= 6:
-                    oi(f"вы выиграли {o*2}!")
+                    oi(f"вы выиграли {o*2}$!")
                     balanse = o*2+balanse
-                    oi(f"ваш баланс = {balanse}")
+                    oi(f"ваш баланс = {balanse}$")
                 else:
-                    oi(f"вы проиграли {o}")
+                    oi(f"вы проиграли {o}$")
                     balanse = balanse - o
-                    oi(f"ваш баланс = {balanse}")
+                    oi(f"ваш баланс = {balanse}$")
             if g == 7:
                 if opp <= 7:
-                    oi(f"вы выиграли {o*2}!")
+                    oi(f"вы выиграли {o*2}$!")
                     balanse = o*2+balanse
-                    oi(f"ваш баланс = {balanse}")
+                    oi(f"ваш баланс = {balanse}$")
                 else:
-                    oi(f"вы проиграли {o}")
+                    oi(f"вы проиграли {o}$")
                     balanse = balanse - o
-                    oi(f"ваш баланс = {balanse}")
+                    oi(f"ваш баланс = {balanse}$")
             if g == 8:
                 if opp <= 8:
-                    oi(f"вы выиграли {o*2}!")
+                    oi(f"вы выиграли {o*2}$!")
                     balanse = o*2+balanse
-                    oi(f"ваш баланс = {balanse}")
+                    oi(f"ваш баланс = {balanse}$")
                 else:
-                    oi(f"вы проиграли {o}")
+                    oi(f"вы проиграли {o}$")
                     balanse = balanse - o
-                    oi(f"ваш баланс = {balanse}")
+                    oi(f"ваш баланс = {balanse}$")
             
 def ui(output):
     console.insert(tk.END, output + "\n")
